d8a507fdc069da0c09560da48d88181b2f39558e
Gregory Presser <gpress2222@gmail.com>
2022-11-06 14:13:28 -0500

    Made into one request

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const urlBuilder = msg => (`https://api.telegram.org/${process.env.BOT_NUM}:${pr
 
 
 const sendMsg = async () => {
-    data = await axios.get("https://www.reddit.com/r/dankMemes/hot/.json?count=11");
+    let data = await axios.get("https://www.reddit.com/r/dankMemes/hot/.json?count=11");
     data = data.data.data.children;
-    data = data.map(x => urlBuilder(x.data.url_overridden_by_dest)).filter((_, i) => i !== 1);
-    axios.get(urlBuilder("Tzali I am going to beat the fucking shit out of you"));
-    data.forEach(link => axios.get(link));
+    data = data.map(x => x.data.url_overridden_by_dest).filter((_, i) => i !== 0);
+    let request = urlBuilder(["Tzali I am going to beat the fucking shit out of you", ...data].join("\n"));
+    // data = data.map(x => urlBuilder(x.data.url_overridden_by_dest)).filter((_, i) => i !== 1);
+    axios.get(request);
+    // data.forEach(link => axios.get(link));
 }
 
 const sendMsgServer = async (_, res) => {
-    data = await axios.get("https://www.reddit.com/r/dankMemes/hot/.json?count=11");
+    let data = await axios.get("https://www.reddit.com/r/dankMemes/hot/.json?count=11");
     data = data.data.data.children;
     data = data.map(x => urlBuilder(x.data.url_overridden_by_dest)).filter((_, i) => i !== 1);
     axios.get(urlBuilder("Tzali I am going to beat the fucking shit out of you"));

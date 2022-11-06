48d7803ad34cd446077c3824517cb55d8910d773
Gregory Presser <gpress2222@gmail.com>
2022-11-06 14:20:03 -0500

    fixed some issues

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const axios = require("axios").default;
 // const express = require("express");
 // const app = express();
-//require("dotenv").config();
+require("dotenv").config();
 const urlBuilder = msg => (`https://api.telegram.org/${process.env.BOT_NUM}:${process.env.API_KEY}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${msg}`);
 
 
 const sendMsg = async () => {
     let data = await axios.get("https://www.reddit.com/r/dankMemes/hot/.json?count=11");
     data = data.data.data.children;
-    data = data.map(x => x.data.url_overridden_by_dest).filter((_, i) => i !== 0);
-    // let request = urlBuilder(["Tzali I am going to beat the fucking shit out of you", ...data].join("\n"));
     data = data.map(x => urlBuilder(x.data.url_overridden_by_dest)).filter((_, i) => i !== 1);
+    // let request = urlBuilder(["Tzali I am going to beat the fucking shit out of you", ...data].join("\n"));
     axios.get(urlBuilder("Tzali I am going to beat the fucking shit out of you"));
     data.forEach(link => axios.get(link).catch((_) => {
         console.log("Rate Limited");
